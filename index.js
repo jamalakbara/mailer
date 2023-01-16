d6a4192022cf43096248f0d7352234bd1d9702a8
akbar <jamalakbaralam@gmail.com>
2023-01-16 22:26:40 +0700

    update get host and port from env

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ const port = process.env.PORT || 5000;
 
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  host: 'smtp.gmail.com',
-  secure: false,
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: true,
   auth: {
     user: process.env.USER_EMAIL,
     pass: process.env.USER_PASSWORD
@@ -37,7 +37,7 @@ app.get('/', (req, res) => {
 app.post('/contact-us', (req, res) => {
   const { from, subject, body, name, company } = req.body;
   const mailOptions = {
-    from,
+    from: `${from} <${process.env.USER_EMAIL}>`,
     to: process.env.USER_EMAIL,
     subject,
     html: `
